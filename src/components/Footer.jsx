6e35ce65121e5068f04e62479dbab09f1c641f17
Shubham Pathak <you@example.com>
2023-08-03 04:05:02 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="http://youtube.com/shubhampathak" target={"blank"}>YouTube</a>
+                <a href="https://www.instagram.com/" target={"blank"}>Instagram</a>
+                <a href="https://www.google.com/search?gs_ssp=eJzj4tLP1TcoqiwoyE1XYDRgdGDwYkvPLMkoTQIAWC4HFA&q=github&oq=git&aqs=chrome.1.69i60j46i39i199i465i650j69i57j0i131i433i512l2j69i60l2j5.3385j0j7&sourceid=chrome&ie=UTF-8" target={"blank"}>GitHub</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
